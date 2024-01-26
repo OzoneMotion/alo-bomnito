13a48b84606774e845235f453d0e25c2247134d2
OzoneMotion <seovelaz@gmail.com>
2024-01-25 23:41:11 -0600

    Cambio en códigos, ya se valida

--- javascript/versionJ.js
+++ javascript/versionJ.js
@@ -6,7 +6,7 @@ const expresiones = {
     // password: /^.{4,12}$/, // 4 a 12 digitos.
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-    caja: /^[a-zA-ZÀ-ÿ\s]{1,400}$/, // Letras, numeros, guion y guion_bajo
+    caja: /^[a-zA-ZÀ-ÿ\s]{1,1000}$/, // Letras, numeros, guion y guion_bajo
 }
 
 const campos = {
