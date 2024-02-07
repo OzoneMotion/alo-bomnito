ff6e0e95208334db7f264d92822d7caf9544dc7f
Christian Hernández <christian.mhm@outlook.com>
2024-02-07 01:51:02 -0600

    Se agregó funcionalidad para ver el modo de uso e ingredientes de cada producto de forma independiente

--- javascript/administrar_prodcutos.js
+++ javascript/administrar_prodcutos.js
@@ -3,6 +3,19 @@ import productos from '../productos.json' assert {type: 'json'};
 let productosID = document.querySelector('.main-container');
 let tablaIngredientes = document.querySelector('.new-cont');
 
+const recuperarIngredientesProducto = (idProducto) => {
+    let ingrendientes = "";
+
+    productos[idProducto - 1].tabla.forEach(elemento => {
+        ingrendientes += `<tr>
+                            <th class="tabl">${elemento.ingrediente}</th>
+                            <th class="tabr">${elemento.funcion}</th>
+                        </tr>`;
+    });
+
+    return ingrendientes;
+}
+
 productos.forEach(item => {
     productosID.innerHTML += `<div class="main-container">
     <div class="col-md-4">
@@ -44,7 +57,7 @@ productos.forEach(item => {
                             gallery_thumbnail
                             </span>
                           </button>
-                          <button class="abrir">
+                          <button class="abrir" id="btn-${item.id}">
                             <span class="material-symbols-outlined">
                             contact_support
                             </span>
@@ -68,14 +81,48 @@ productos.forEach(item => {
   </div>`
 });
 
+let mods = "";
+productos.forEach(producto => {
+    mods += `<div class="mod-cont" id="mod_contenedor-${producto.id}">
+    <div class="mod" id="mod-${producto.id}">
+    <h1 class="cont-text"></h1>
 
+    <ul class="accordion">
+
+        <li>
+            <input type="radio" name="accordion" id="first-${producto.id}" checked>
+                <label for="first-${producto.id}">Modo de uso</label>
+                <div class="content">
+                    <p>${producto.uso}</p>
+                </div>
+        </li>
+
+        <li>
+            <input type="radio" name="accordion" id="second-${producto.id}">
+                <label for="second-${producto.id}">Ingredientes</label>
+                <div class="content">
+                    <table>
+                        <tbody>
+                            ${recuperarIngredientesProducto(producto.id)}
+                        </tbody>
+                    </table>
+                </div>
+        </li>
+
+    </ul>
+
+    <button class="cerrar" id="cerrar-${producto.id}">Cerrar</button>
+</div>
+</div>`
+});
+
+// console.log(mods);
 
 const openButtons = document.querySelectorAll('.open');
 const mod_cont = document.getElementById('mod_cont');
 const close = document.getElementById('close');
 const abrirButtons = document.querySelectorAll('.abrir');
-const mod_contenedor = document.getElementById('mod_contenedor');
-const cerrar = document.getElementById('cerrar');
+const modContainer = document.querySelector('.mod-container');
 
 openButtons.forEach(open => {
     open.addEventListener('click', () => {
@@ -83,8 +130,13 @@ openButtons.forEach(open => {
     });
 });
 
-abrirButtons.forEach(abrir => {
+modContainer.insertAdjacentHTML('beforeend', mods);
+
+abrirButtons.forEach(abrirBtn => {
+    const abrir = document.getElementById(abrirBtn.id);
+
     abrir.addEventListener('click', () => {
+        const mod_contenedor = document.getElementById(`mod_contenedor-${abrirBtn.id.split('-')[1]}`);
         mod_contenedor.classList.add('show');
     });
 });
@@ -93,6 +145,11 @@ close.addEventListener('click', () => {
     mod_cont.classList.remove('show');
 });
 
-cerrar.addEventListener('click', () => {
-    mod_contenedor.classList.remove('show');
+const cerrarButtons = document.querySelectorAll('.cerrar');
+
+cerrarButtons.forEach(cerrarBtn => {
+    cerrarBtn.addEventListener('click', () => {
+        const mod_contenedor = document.getElementById(`mod_contenedor-${cerrarBtn.id.split('-')[1]}`);
+        mod_contenedor.classList.remove('show');
+    });
 });
