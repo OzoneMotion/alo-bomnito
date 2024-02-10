6f503c0fb084ae6dafa18f6fea6a60b24930639b
Christian Hernández <christian.mhm@outlook.com>
2024-02-09 23:21:40 -0600

    Archivos esenciales de Contactanos

--- javascript/popup.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#open').on("click", function () {
-        $("#popup").fadeIn("slow");
-    });
-
-    $('#close').on("click", function () {
-        $("#popup").fadeOut("slow");
-    });
-})
