a722a70cd28e112b0c9ef866c7e897a9efd416a1
aridai perez <aridaiperez03@gmail.com>
2024-02-18 19:40:34 -0800

    estilos y funcionalidades a la pantalla productos, con el carrito

--- javascript/productos.js
+++ javascript/productos.js
@@ -1,16 +1,10 @@
 import data from '../productos.json' assert {type: 'json'}
 
-//import productos from '../productos.json' assert {type: 'json'}
-
-
 let contenedorCards = document.querySelector('#contenedor-cards');
-// let miniCarrusel = document.querySelector('#mini-carrusel');
-// let miniBack = document.getElementById('miniBack');
-// let miniForward = document.getElementById('miniForward');
-// let miniProductos = [];
 
 
 data.forEach((element, index)=> {
+
   let imagen1 =element.imagen1[0];
   let imagenUrl1= imagen1 ? Object.values(imagen1)[0]:'';
 
@@ -23,124 +17,60 @@ data.forEach((element, index)=> {
   //Crear la card con el html correspondiente
     contenedorCards.innerHTML += `
     <ul class="products">
-    <li>
+    <li class="producto">
       <div class="contenedor-img">
-      <img  class="img-card" alt="Cambiar imagen"  onmouseout="this.src='${imagenUrl1}';" onmouseover="this.src='${imagenUrl2}';" src="${imagenUrl3}" />
+      <img class="img-card" alt="Cambiar imagen"  onmouseout="this.src='${imagenUrl1}';" onmouseover="this.src='${imagenUrl2}';" src="${imagenUrl3}" />
       </div>
       <div class="info-produc">
-        <h1>${element.nombre}</h1>
-        <p class="products-name" data-index="${index}" id="${element.id}">${element.marca}</p>
+        <p class="productoNombre" id="${element.id}">${element.nombre}</p>
+        <p class="products-name" data-index="${index}">${element.marca}</p>
         <p class="products-mlgr">${element.contenido}</p>
         <div class="container-agregar">
           <p class="card-precio">$${element.precio}</p>
-          <a href="../html/carrito_de_compras.html"><i class="fa-solid fa-plus" id="icon-card"></i></a>
+          <div class="${element.id}" id="addElement"><i class="fa-solid fa-plus" id="icon-card"></i></div>
         </div>
       </div>
     </li>
 </ul>`
 
-//producto(imagenUrl1,imagenUrl2,imagenUrl3, element)
-
-// let mini = document.createElement('div');
-// mini.classList.add('mini-mini');
-// mini.innerHTML +=
-// `<div class="tarjeta-mini">
-// <div id="image">
-// <img class="image-mini" alt="Imagen mini" onmouseout="this.src='${imagenUrl1}';" onmouseover="this.src='${imagenUrl2}';" src="${imagenUrl3}"/>
-// </div>
-// <div id="information" class="information-text">
-//     <p class="mini-nombre">${element.nombre}</p>
-//     <p class="mini-texty">${element.marca}</p>
-//     <p class="mini-contenido">${element.contenido}</p>
-//     <p class="mini-texty">$${element.precio}.00</p>
-// </div>
-// </div>`;
-// miniProductos.push(mini);
 });
 
 
-// let inicio = 0;
-
-// function actualizarProducto() {
-//     miniCarrusel.innerHTML = '';
-//     let cantidadAMostrar;
-
-//     if (window.innerWidth < 744) {
-//         cantidadAMostrar = 2;
-//     } else if (window.innerWidth < 992) {
-//         cantidadAMostrar = 2;
-//     } else {
-//         cantidadAMostrar = 4;
-//     }
-//     for (let i = 0; i < cantidadAMostrar; i++) {
-//         let index = (inicio + i) % miniProductos.length;
-//         miniCarrusel.appendChild(miniProductos[index]);
-//     }
-// }
-
-// function ajustarInicio(desplazamiento) {
-//     let cantidadAMostrar;
-
-//     if (window.innerWidth < 744) {
-//         cantidadAMostrar = 2;
-//     } else if (window.innerWidth <992) {
-//         cantidadAMostrar = 2;
-//     } else {
-//         cantidadAMostrar = 4;
-//     }
-
-//     inicio = (inicio + desplazamiento + miniProductos.length) % miniProductos.length;
-
-//     while (inicio > miniProductos.length - cantidadAMostrar) {
-//         inicio -= cantidadAMostrar;
-//     }
-// }
-
-// miniBack.addEventListener('click', function() {
-//     ajustarInicio(-1);
-//     actualizarProducto();
-// });
-
-// miniForward.addEventListener('click', function() {
-//     ajustarInicio(1);
-//     actualizarProducto();
-// });
-
-// window.addEventListener('resize', function() {
-//     ajustarInicio(0);
-//     actualizarProducto();
-// });
-
-// ajustarInicio(0);
-// actualizarProducto();
-
-
-
-document.querySelectorAll('.products-name').forEach(item => {
+document.querySelectorAll('#addElement').forEach(item => {
   item.addEventListener('click', function() {
-    //alert("hola")
-    console.log("hola")
-    let ides = item.id -1;
-    console.log(ides)
-    producto(ides)
-    //const aidi = document.getElementById("document")
-    
-    //console.log(produc)
-    
-    //const modal = document.getElementById("modal")
-    
-    modal.showModal();
-    // let index = parseInt(this.getAttribute('data-index'));
-    // let producto = productos[index];
-    // let url = '../html/producto_individual.html'; // Reemplaza 'ruta_del_archivo_del_segundo_codigo.html' por la ruta correcta del segundo código
-    
-    // // Abrir en una nueva pestaña
-    // window.open(url, '_blank');
+    let indexProducto = item.className - 1
+    agregarAlCarrito(indexProducto)
   });
 });
 
-let back = document.getElementById('back');
-let forward = document.getElementById('forward');
+function agregarAlCarrito(indexProducto){
+
+  const nombre = data[indexProducto].nombre;
+  const marca = data[indexProducto].marca;
+  const precio = data[indexProducto].precio;
+  const imagen = data[indexProducto].imagen1[0]
+  let cantidad = 1
+
+  const productosCarrito = JSON.parse(localStorage.getItem('productosCarrito')) || []
+
+  productosCarrito.push({nombre : nombre, marca : marca, precio : precio, imagen : imagen, cantidad : cantidad})
+  localStorage.setItem('productosCarrito', JSON.stringify(productosCarrito))
+  alert("Se agrego el producto al carrito")
+
+}
+
+document.querySelectorAll('.productoNombre').forEach(item => {
+  item.addEventListener('click', function() {
+    let ides = item.id -1;
+    console.log(ides)
+    producto(ides)
+    
+    modal.showModal();
+  });
+});
+
+
+
 let img = document.getElementById('img');
 let information = document.querySelector('#info');
 let accordionDesc = document.querySelector('#accordionDesc');
@@ -149,9 +79,9 @@ let accordionIngred = document.querySelector('#accordionIngred');
 let tarjetaDescripcion = document.querySelector('#tarjetaDescripcion');
 let tarjetaIndicaciones = document.querySelector('#tarjetaIndicaciones');
 let tarjetaIngredientes = document.querySelector('#tarjetaIngredientes');
-let actual = 0;
 
 function producto(ides){
+
   console.log(ides)
   console.log(data[ides])
 
@@ -166,31 +96,56 @@ function producto(ides){
 
 
 
-  img.innerHTML = `<img src="${actual === 0 ? imagenUrl1 : (actual === 1 ? imagenUrl2 : imagenUrl3)}" alt="imagen producto">`;
+  img.innerHTML = `<div id="carouselExampleDark" class="carousel carousel-dark slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="${imagenUrl1}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+    <img src="${imagenUrl2}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+    <img src="${imagenUrl3}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>`;
   
   information.innerHTML = 
-  `<p class="nombre">${data[ides].nombre}</p>
-  <p>${data[ides].marca}</p>
-  <p>${data[ides].contenido}</p>
-  <p class="precio">$${data[ides].precio}.00</p>
+  `<p class="nombre parrafo">${data[ides].nombre}</p>
+  <p class="parrafo">${data[ides].marca}</p>
+  <p class="parrafo">${data[ides].contenido}</p>
+  <p class="precio parrafo">$${data[ides].precio}.00</p>
   <div class="button-carrito">
-              <button id="add-car" onclick="location.href=''">Agregar al carrito</button>
-              </div>`;
-
-              tarjetaDescripcion.innerHTML = 
-              `<p>${data[ides].descripcion}</p>`;
-              
+  <button id="add-car" class="${data[ides].id}"">Agregar al carrito</button>
+  </div>`;
+  
+  tarjetaDescripcion.innerHTML = 
+  `<p class="parrafo">${data[ides].descripcion}</p>`;
+  
               tarjetaIndicaciones.innerHTML = 
-              `<p>${data[ides].uso}</p>`;
+              `<p class="parrafo">${data[ides].uso}</p>`;
               
               tarjetaIngredientes.innerHTML = 
               recuperarIngredProducto(data[ides]);
               
               accordionDesc.innerHTML = 
-              `<p>${data[ides].descripcion}</p>`;
+              `<p class="parrafo">${data[ides].descripcion}</p>`;
 
   accordionIndica.innerHTML = 
-  `<p>${data[ides].uso}</p>`;
+  `<p class="parrafo">${data[ides].uso}</p>`;
   
   accordionIngred.innerHTML = 
   recuperarIngredProducto(data[ides]);
@@ -208,22 +163,17 @@ const recuperarIngredProducto = (producto) => {
   return ingredientes;
 };
 
+document.querySelectorAll('#add-car').forEach(item => {
+  item.addEventListener('click', function() {
+    //let indexProducto = item.className - 1
+    console.log("hola")
+    //agregarAlCarrito(indexProducto)
+  });
+});
 
-// back.addEventListener('click', function() {
-//   actual -= 1;
 
-//   if (actual === -1) {
-//       actual = 2;
-//   }
-//   producto();
-// });
+const btnClose = document.getElementById("btnClose")
 
-// forward.addEventListener('click', function() {
-//   actual += 1;
-
-//   if (actual === 3) {
-//       actual = 0;
-//   }
-//   actualizarImagen();
-// });
-// actualizarImagen();
+btnClose.addEventListener("click", function () {
+  modal.close();
+});
