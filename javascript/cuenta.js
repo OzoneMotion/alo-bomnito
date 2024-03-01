a98505a1985fc48499eb2597f790e244bed812e3
aridai perez <aridaiperez03@gmail.com>
2024-03-01 01:21:18 -0800

    inicio y cerrar sesion

--- javascript/cuenta.js
+++ javascript/cuenta.js
@@ -12,6 +12,7 @@ async function imprimirUsuario(idCliente) {
     nameId.innerHTML = `${usuariosLocales.nameId}`;
     emailId.innerHTML = `${usuariosLocales.nameId}`;
     mostrarContrasenia(usuariosLocales)
+    verificar()
 }
 
 function mostrarContrasenia(usuario) {
@@ -49,6 +50,36 @@ function Visibility(){
     }
 
 }
+
+function verificar() {
+    const cerrarSes = document.getElementById("cerrarSes")
+    const cerrarSesion = document.getElementById("cerrarSesion")
+    const cuentitaIniciada = document.getElementById("cuentitaIniciada")
+    const cuentita = document.getElementById("cuentita")
+    const cuentaIconDesktop = document.getElementById("cuentaIconDesktop")
+
+    if (localStorage.getItem('usuarioActivo') == null){
+
+     } else{
+        cuentitaIniciada.classList.remove("desactive")
+        cuentita.classList.remove("desactive_desktop")
+        cerrarSesion.classList.remove("desactive")
+        cuentaIconDesktop.href = "./cuenta.html"
+        cuentitaIniciada.classList.add("desactive_desktop")
+        cuentita.classList.add("desactive")
+     }
+
+    cerrarSesion.addEventListener('click', () => {
+        localStorage.clear();
+    });
+    
+    cerrarSes.addEventListener('click', () => {
+        localStorage.clear();
+        window.location = "index.html"    
+    });
+
+}
+
 
 const getData = async () => {
     try {
